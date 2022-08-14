2f96bce8c06513eb6e99f7927257fd7062a5de9e
Nicola Baroncini <Nicola.Baroncini@leonardo.com>
2022-08-14 14:09:54 +0200

    Activity Confortable Simulato OK

---
+++ StartTestSimulazioni.bat
@@ -0,0 +1,4 @@
+cd C:\cygwin\bin
+echo "ci sono"
+start /B C:\Temp\EfficiencyPlanMBSE\TestSimulazioni\DefaultComponent\ActvityDiagramCalculator\DefaultComponent.exe
+C:\Temp\EfficiencyPlanMBSE\TestSimulazioni\DefaultComponent\StattDiagWithPAnel\DefaultComponent.exe
