fc81c5a04b55331a81e317ca491ed01bd80d86e3
BARONCININ <Nicola.Baroncini@leonardo.com>
2022-09-06 17:14:39 +0200

    Cancelalzione

--- StartTestSimulazioni.bat
+++
@@ -1,4 +0,0 @@
-cd C:\cygwin\bin
-echo "ci sono"
-start /B C:\Temp\EfficiencyPlanMBSE\TestSimulazioni\DefaultComponent\ActvityDiagramCalculator\DefaultComponent.exe
-C:\Temp\EfficiencyPlanMBSE\TestSimulazioni\DefaultComponent\StattDiagWithPAnel\DefaultComponent.exe
